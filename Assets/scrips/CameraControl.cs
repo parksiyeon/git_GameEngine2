720fa7fcf24738eade307bf5f1e46efd1748da06
Park Si Yeon <parksiyeon1020@gmail.com>
2020-11-28 13:27:21 +0900

    바닥 밑 기본적인 움직임

---
+++ Assets/scrips/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+    private Vector3 velos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(0f, 1f, -5f);
+        velos = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.localPosition);
+
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPosition, ref velos, 0.2f);
+
+        //transform.localPosition = Vector3.Lerp(transform.localPosition, target.localPosition + offset , 0.5f);
+        // 원래 위치, 목표 위치, t ??
+    }
+}
---
+++ Assets/scrips/PlayerControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using TreeEditor;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed;
+
+    float hAxis;
+    float vAxis;
+
+
+    Vector3 moveVec;
+
+
+    private void Awake()
+    {
+     
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+    }
+
+
+    private void LateUpdate()
+    {
+        
+    }
+
+
+}
